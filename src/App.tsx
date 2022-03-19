42a3594a3d80e83e71736ba2d50e034f4c76bea2
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-03-19 22:17:57 +0530

    Library implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+// @ts-ignore
+import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
 
 function App() {
+
+  const [recordState, setRecordState] = useState(null)
+
+ const start = () => {
+    setRecordState(RecordState.START)
+  }
+ 
+  const stop = () => {
+    setRecordState(RecordState.STOP
+    )
+  }
+ const onStop = (audioData:any) => {
+    console.log('audioData', audioData)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <AudioReactRecorder canvasWidth={1000} state={recordState} onStop={onStop} />
+ 
+        <button onClick={start}>Start</button>
+        <button onClick={stop}>Stop</button>
+      </div>
     </div>
   );
 }
